d923dc9c23545504e229544d47803582e3b604c2
minwoo2500 <42472602+Yoon0511@users.noreply.github.com>
2024-10-18 19:09:30 +0900

    dtd

---
+++ Project_A/Project_A/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Project_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 10;
+            int atk = 10;
+            int PlayerCount = 2;
+            bool GameLoop = true;
+
+            while(GameLoop)
+            {
+                if(hp == 0)
+                {
+                    if (PlayerCount > 0)
+                    {
+                        hp = 10;
+                        PlayerCount--;
+                    }
+                    else
+                        GameLoop = false;
+                }
+            }
+           
+        }
+    }
+}
